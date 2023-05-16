4d3b98d171f9d593f086565bd63920784da80540
Talantone <talgatsitd@gmail.com>
2023-05-16 14:52:04 +0200

    corrected config

--- core/config.py
+++ core/config.py
@@ -2,8 +2,8 @@ from starlette.config import Config
 
 config = Config(".env")
 
-DB_URL = config("DB_URL", cast=str, default="localhost:5432")
-TEST_DB_URL = config("DB_URL", cast=str, default='localhost:5433')
+DB_URL = config("DB_URL", cast=str, default="postgresql://root:qwerty@localhost:5432/product-db")
+TEST_DB_URL = config("DB_URL", cast=str, default='postgresql://root:qwerty@localhost:5433/product-test-db')
 ACCESS_EXPIRATION = config("ACCESS_EXPIRATION", cast=int, default=60)
 APP_PORT = config("APP_PORT", cast=int, default=8000)
 APP_ADDR = config("APP_ADDR", cast=str, default="localhost")
